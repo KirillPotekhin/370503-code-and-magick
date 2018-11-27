fe522f35977f811625fa4238dffbcbc71bd57e68
kirillpotekhin <kirill.potekhin@gmail.com>
2018-11-27 18:48:25 +0700

    Установил скорость фаербола, его размер, скорость волшебника, его ширину и высоту

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var fireballSize = 22;
+
+var getFireballSpeed = function(left) {
+  return left ? 2 : 5
+}
+
+wizardSpeed = 3;
+
+wizardWidth = 70;
+
+var getWizardHeight = function() {
+  return 2.337 * wizardWidth;
+}
