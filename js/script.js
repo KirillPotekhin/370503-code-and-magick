bdd76775a5be181a249ab066ebf231c49d11767f
kirillpotekhin <kirill.potekhin@gmail.com>
2018-11-28 18:36:25 +0700

    Частично исправил ошибки по тревису, остались ошибки из-за того что не использую переменные, которые задал

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
+'use strict';
+
 var fireballSize = 22;
 
-var getFireballSpeed = function(left) {
-  return left ? 2 : 5
-}
+var getFireballSpeed = function (left) {
+  return left ? 2 : 5;
+};
 
 wizardSpeed = 3;
 
 wizardWidth = 70;
 
-var getWizardHeight = function() {
+var getWizardHeight = function () {
   return 2.337 * wizardWidth;
-}
+};
 
-var getWizardX = function(width) {
+var getWizardX = function (width) {
   return (width * 0.5) - (wizardWidth * 0.5);
-}
+};
 
-var getWizardY = function(height) {
+var getWizardY = function (height) {
   return (height - getWizardHeight()) * (1 - 2 / 3);
-}
+};
