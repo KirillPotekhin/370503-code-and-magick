ef8cab704d37d5ed89b58b92a818efa5c02d08a6
kirillpotekhin <kirill.potekhin@gmail.com>
2018-11-28 18:43:32 +0700

    Объявил все переменные и функции через var

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ var getFireballSpeed = function (left) {
   return left ? 2 : 5;
 };
 
-wizardSpeed = 3;
+var wizardSpeed = 3;
 
-wizardWidth = 70;
+var wizardWidth = 70;
 
 var getWizardHeight = function () {
   return 2.337 * wizardWidth;
